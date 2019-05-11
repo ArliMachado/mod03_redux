95eb0f1615d99e44f93742ea53489e4180832be4
Arli Machado <arli.machado@gmail.com>
2019-05-11 19:33:20 -0300

    Removendo arquivos não utilizados

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 import store from './store/index';
-import TodoList from './TodoList';
+import Routes from './routes';
 
 const App = () => (
   <Provider store={store}>
-    <TodoList />
+    <Routes />
   </Provider>
 );
 
